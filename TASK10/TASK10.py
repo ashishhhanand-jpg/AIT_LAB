c79020facfdc7ac71635a39cf2934ad8c1385e5a
ashishhhanand-jpg <ashishhhanand@gmail.com>
2025-11-06 10:34:58 +0530

    Create TASK10.py

---
+++ TASK10/TASK10.py
@@ -0,0 +1,30 @@
+facts = [
+    "john_is_cold.",           
+    "raining.",                
+    "john_Forgot_His_Raincoat.",  
+    "fred_lost_his_car_keys.", 
+    "peter_footballer."        
+]
+
+
+def verify_fact(fact):
+    fact = fact.rstrip(".")
+    # Perform some logic to verify the fact
+    if fact == "john_Forgot_His_Raincoat":
+        return True
+    elif fact == "raining":
+        return True
+    elif fact == "foggy":
+        return True
+    elif fact == "Cloudy":
+        return False  # Assume it's not cloudy
+    else:
+        return False
+
+# Verify each fact
+for fact in facts:
+    if verify_fact(fact):
+        print(f"{fact} - Yes")
+    else:
+        print(f"{fact} - No")
+      
